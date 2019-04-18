bdd7e190ae767f2cbc9b44da83e4366bcf23955c
Tykun <tykun.khy@hotmail.com>
2019-04-18 12:07:30 -0500

    first commit

---
+++ src/components/FormPersonelDetails.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import { RaisedButton } from 'material-ui';
+
+class FormPersonelDetails extends Component {
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
+
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+  render() {
+    const { values, handleChange } = this.props;
+    return (
+      <MuiThemeProvider>
+        <React.Fragment>
+          <AppBar title="Enter Personel Details" />
+          <TextField
+            hintText="Enter Your Occupation"
+            floatingLabelText="Occupation"
+            onChange={handleChange('occupation')}
+            defaultValue={values.occupation}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your City"
+            floatingLabelText="City"
+            onChange={handleChange('city')}
+            defaultValue={values.city}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your bio"
+            floatingLabelText="bio"
+            onChange={handleChange('bio')}
+            defaultValue={values.bio}
+          />
+          <br />
+          <RaisedButton
+            label="Back"
+            primary={false}
+            style={style.button}
+            onClick={this.back}
+          />
+          <RaisedButton
+            label="Continue"
+            primary={true}
+            style={style.button}
+            onClick={this.continue}
+          />
+        </React.Fragment>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+const style = {
+  button: {
+    margin: 15
+  }
+};
+
+export default FormPersonelDetails;
